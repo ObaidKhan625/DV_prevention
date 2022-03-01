919d0f61d8f918c11867644498dc53b8778573ad
Obaid <ephiram2002@gmail.com>
2022-03-01 14:04:51 +0530

    Added some frontend

--- user_requests/views.py
+++ user_requests/views.py
@@ -15,7 +15,7 @@ def verifyUser(request, profile_id):
 	"""
 	verified_user = User.objects.get(id = profile_id)
 	if(verified_user == request.user):
-		return
+		return JsonResponse({'verification_post':False})
 	if(Verification.objects.filter(verified_user = verified_user, verified_by = request.user).exists()):
 		return JsonResponse({'verification_post':False})
 	else:
@@ -30,7 +30,7 @@ def reportUser(request, profile_id):
 	"""
 	reported_user = User.objects.get(id = profile_id)
 	if(reported_user == request.user):
-		return
+		return JsonResponse({'verification_post':False})
 	if(Report.objects.filter(reported_user = reported_user, reported_by = request.user).exists()):
 		return JsonResponse({'report_post':False})
 	else:
